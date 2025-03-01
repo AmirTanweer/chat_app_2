da4f6bcff53961f2d621e74469f05d97a6b970b2
AmirTanweer <amirtanweer0@gmail.com>
2025-03-01 14:31:43 +0530

    Authentication route is completed

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express=require('express');
 const connectDB = require('./db');
+const cors=require('cors')
+const UserRoute=require('./routes/User.Route')
 const app=express();
 const port=5000;
-
+app.use(express.json());
+app.use(cors())
 app.get('/',(req,res)=>{
     res.send("hello kdsla world this is amir");
 })
+app.use('/api/auth',UserRoute)
 connectDB()
 app.listen(port,()=>{
     console.log("Server is running on Port ",port);
